7808db926726ae55fdb18b8838f46e2065b158c8
David Sanders <sanderdb01@gmail.com>
2023-09-07 13:43:46 -0400

    Initial Commit

---
+++ VideoPlayerApp/VideoPlayerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerAppApp.swift
+//  VideoPlayerApp
+//
+//  Created by David Sanders on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
